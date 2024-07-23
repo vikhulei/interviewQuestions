d43308bbf31ebbb80a5940c3f4011178e5e6794a
vikhulei <vikhulei@gmail.com>
2024-07-23 06:06:54 +0100

    js tasks ordered

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// Capitalize first letter
+
+
+
 //1.ADD TWO NUMBERS
 
+
 //2.ADD UNLIMITED NUMBER OF NUMBERS
+
 
 //3.CHECK IF STRING B IS IN STRING A
 
@@ -9,53 +15,12 @@
 //5.COMBINE ONLY NUMBERIC VALUES FROM TWO ARRAYS
 
 //6.RETURN THE LONGEST STRING FROM AN ARRAY
-const res = (arr1) => {
-  return arr1.reduce((a, b) => {
-    if (a.length > b.length) {
-      return a;
-    } else {
-      return b;
-    }
-  }, "");
-};
 
-console.log(res(["one", "hello", "hi", "fibonacchi", 5]));
+
 
 //6a. Sort an array
 
 //7. PROCESS FACTORIAL FOR EACH VALUE OF THE ARRAY AND PUT IT IN NEW ARRAY
-
-//12. MAXIMUM INTERVAL BETWEEN 2 CONSEQUTIVE NUMBERS
-
-// const test = (arr) => {
-//   let newArr = [];
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     newArr.push(arr[i + 1] - arr[i]);
-//   }
-//   return newArr;
-// };
-
-// console.log(test([3, 5, 7, 17]));
-
-//13. RETURN SMALLEST NUMBER OF ARRAY
-
-//10. THE SUM OF LENGTHS OF ANY NUMBER OF STRINGS
-
-//20. Function takes array of objects and returns only the property of the "name" key, if it exists
-
-//21. FIZZBUZ
-
-//21. REVERSE A STRING/WORD
-
-//22. CHECK IF VALUE IS OBJECT/ARRAY
-
-//23 CHECK IF NUMBER IS INTEGER
-
-//24. MUL FUNCTION
-
-//25. FIBONACCHI
-
-// 25. Write a closure to console log "Hi John" and "Hello John"
 
 //8. WRITE THE AGE FUNCTION:
 // - if the age value has not been set, you need to create the following error: "Please, enter your age",
@@ -68,7 +33,13 @@ console.log(res(["one", "hello", "hi", "fibonacchi", 5]));
 
 //9. The function receives an array of integers, a number and a parameter (greater, less). Print a new array, where all elements will be greater/less than this number. By default, the number is 0, the parameter is greater.
 
+//10. THE SUM OF LENGTHS OF ANY NUMBER OF STRINGS
+
 //11. HOW MANY SECONDS in the number of seconds, minutes, hours, days, weeks, months, years
+
+//12. MAXIMUM INTERVAL BETWEEN 2 CONSEQUTIVE NUMBERS
+
+//13. RETURN SMALLEST NUMBER OF ARRAY
 
 //14. Create a Movie class, the constructor of which accepts 3 parameters: movie name name, movie genre category and start year of startShow. The class has a watchMovie() method that returns a phrase and adds a movie name name parameter to it at the end. For example, "I watch the movie Titanic, which is drama, produced in 1997".
 
@@ -80,19 +51,6 @@ console.log(res(["one", "hello", "hi", "fibonacchi", 5]));
 // Create an instance of class stud1 named 'Ivan Petrenko' and direction 'web'.
 // Create an instance of class stud2 named 'Sergiy Koval' and direction 'python'.
 // Create an instance of the stud3 class named ‘Ihor Kohut’ and the direction ‘qc’ using the static studentBuilder() method.
-
-// class Student {
-//   constructor(fullName, direction) {
-//     this.fullName = fullName;
-//     this.direction = direction;
-//   }
-//   showFullName() {
-//     return this.fullName;
-//   }
-//   nameIncludes(data) {
-//     return this.fullName.includes(data);
-//   }
-// }
 
 //16. Write a mapCreator(keys, values) function that takes two arrays of equal length. Using these arrays, the function must create an object of type Map, the keys of which are the values from the first array keys, and the values of Map - the values from the second array values. Moreover, the values of the map elements can be only string values. The function returns this Map object.
 
@@ -110,17 +68,24 @@ console.log(res(["one", "hello", "hi", "fibonacchi", 5]));
 // a
 // b
 
-//20. Write to console with IIFE
+//20. Function takes array of objects and returns only the property of the "name" key, if it exists
 
-//21. Use This to apply method from one object to another
+//21. FIZZBUZ
 
-//22. Async await with throw, try, catch, finally
+//22. REVERSE A STRING/WORD
 
-// let fruit = "oranges";
+//23. CHECK IF VALUE IS OBJECT/ARRAY
 
-// const logFruit = {
-//   oranges: "hello",
-//   apples: () => console.log("apples")
-// };
+//24. CHECK IF NUMBER IS INTEGER
 
-// logFruit["oranges"];
+//25. MUL FUNCTION
+
+//26. Write a closure to console log "Hi John" and "Hello John"
+
+//27. FIBONACCHI
+
+//28. Write to console with IIFE
+
+//29. Use This to apply method from one object to another
+
+//30. Async await with throw, try, catch, finally
